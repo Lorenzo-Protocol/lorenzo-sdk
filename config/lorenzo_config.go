816b02ff1e0995a13e46f1559fe92678255aa35f
0xajing <jing@lorenzo-protocol.xyz>
2024-03-29 13:46:13 +0800

    update go.mod

--- config/lorenzo_config.go
+++ config/lorenzo_config.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"github.com/cosmos/relayer/v2/relayer/chains/cosmos"
 	"net/url"
-	"os"
-	"path/filepath"
 	"time"
 )
 
@@ -15,7 +13,6 @@ type LorenzoConfig struct {
 	Key            string        `mapstructure:"key"`
 	ChainID        string        `mapstructure:"chain-id"`
 	RPCAddr        string        `mapstructure:"rpc-addr"`
-	GRPCAddr       string        `mapstructure:"grpc-addr"`
 	AccountPrefix  string        `mapstructure:"account-prefix"`
 	KeyringBackend string        `mapstructure:"keyring-backend"`
 	GasAdjustment  float64       `mapstructure:"gas-adjustment"`
@@ -57,36 +54,4 @@ func (cfg *LorenzoConfig) ToCosmosProviderConfig() cosmos.CosmosProviderConfig {
 		OutputFormat:   cfg.OutputFormat,
 		SignModeStr:    cfg.SignModeStr,
 	}
-}
-
-func DefaultLorenzoConfig() LorenzoConfig {
-	return LorenzoConfig{
-		Key:     "node0",
-		ChainID: "chain-test",
-		// see https://docs.cosmos.network/master/core/grpc_rest.html for default ports
-		// TODO: configure HTTPS for Lorenzo's RPC server
-		// TODO: how to use Cosmos SDK's RPC server (port 1317) rather than Tendermint's RPC server (port 26657)?
-		RPCAddr: "http://localhost:26657",
-		// TODO: how to support GRPC in the Lorenzo client?
-		GRPCAddr:       "https://localhost:9090",
-		AccountPrefix:  "bbn",
-		KeyringBackend: "test",
-		GasAdjustment:  1.2,
-		GasPrices:      "0.01lrz", //TODO: use lorenzo token symbol
-		KeyDirectory:   defaultLorenzoHome(),
-		Debug:          true,
-		Timeout:        20 * time.Second,
-		OutputFormat:   "json",
-		SignModeStr:    "direct",
-	}
-}
-
-// defaultLorenzoHome returns the default Lorenzo node directory, which is $HOME/.lorenzo
-func defaultLorenzoHome() string {
-	userHomeDir, err := os.UserHomeDir()
-	if err != nil {
-		panic(err)
-	}
-
-	return filepath.Join(userHomeDir, ".lorenzo")
 }
