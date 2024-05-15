93f25f59f01642fe38b900bda37d62a18c64171c
user <163304975+secp256r1@users.noreply.github.com>
2024-05-15 18:04:05 +0800

    feat: update amount parsing

--- event/btcstaking.go
+++ event/btcstaking.go
@@ -89,8 +89,7 @@ func NewBurnEvent(event abci_types.Event) (*BurnEvent, error) {
 	for _, attr := range event.Attributes {
 		value := strings.Trim(attr.Value, "\"")
 		if attr.Key == "amount" {
-			var coin sdk.Coin
-			err = json.Unmarshal([]byte(value), &coin)
+			err = json.Unmarshal([]byte(value), &amount)
 			if err != nil {
 				return nil, err
 			}
