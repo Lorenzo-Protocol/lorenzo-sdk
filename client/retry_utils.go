cdc35b0d0d39ca8ca710ccf8b50c5994018855e7
0xajing <jing@lorenzo-protocol.xyz>
2024-08-15 11:13:42 +0800

    feat:can set client retry configs

--- client/retry_utils.go
+++ client/retry_utils.go
@@ -16,6 +16,19 @@ var (
 	rtyErr    = retry.LastErrorOnly(true)
 )
 
+func (c *Client) SetRetryAttempts(retryNumber uint) {
+	rtyAttNum = retryNumber
+	rtyAtt = retry.Attempts(rtyAttNum)
+}
+
+func (c *Client) SetRetryDelay(milliseconds int) {
+	rtyDel = retry.Delay(time.Millisecond * time.Duration(milliseconds))
+}
+
+func (c *Client) SetIsReturnLatestErrorOnly(isReturnLatestErrorOnly bool) {
+	rtyErr = retry.LastErrorOnly(isReturnLatestErrorOnly)
+}
+
 func errorContained(err error, errList []*errors.Error) bool {
 	for _, e := range errList {
 		if strings.Contains(err.Error(), e.Error()) {
